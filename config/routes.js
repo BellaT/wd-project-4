46e490c4605ee42344b48f64832d8168a1da7302
BellaT <bella_taylor@hotmail.co.uk>
2016-05-29 17:48:29 +0100

    almost have the satellizer auth working

---
+++ config/routes.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    router  = express.Router();
+
+var usersController = require('../controllers/usersController');
+
+router.route('/')
+  .get(usersController.usersIndex);
+
+router.route('/users')
+  .get(usersController.usersIndex)
+  .post(usersController.usersCreate);
+
+router.route('/users/:id')
+  .get(usersController.usersShow)
+  .patch(usersController.usersUpdate)
+  .delete(usersController.usersDelete);
+
+module.exports = router;
